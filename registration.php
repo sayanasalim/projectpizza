93de9d6f1034bc2417ff95be10828ee0037b9b90
sayanasalim <37173954+sayanasalim@users.noreply.github.com>
2019-06-04 10:53:05 +0530

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,96 @@
+
+	
+
+
+<!-- Meta tag Keywords -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Flat Sign Up Form Responsive Widget Template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- Meta tag Keywords -->
+<!-- css files -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
+<script src="jquery.min.js"></script>
+<!-- //css files -->
+<!-- online-fonts -->
+<link href='//fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'><link href='//fonts.googleapis.com/css?family=Raleway+Dots' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<!--header-->
+	<div class="header-w3l">
+		<h1></h1>
+	</div>
+<!--//header-->
+  
+
+
+</head>
+<body>
+<div id="page-wrap">
+
+
+<div id="header">
+<a href="http://www.jquery4u.com/"><img src="http://www.jquery4u.com/wp-content/uploads/2011/07/new-jquery-logo3.png" id="logo-image"></a>
+</div>
+<!--main-->
+<div class="main-agileits">
+	<h2 class="sub-head">REGISTRATION</h2>
+		<div class="sub-main">	
+			<form action="" method="post"  >
+			<p id="vfname" style="color:red;"></p>
+				<input placeholder="pizza name" name="pizza name" id="pizza name" class="name" type="text" required="" pattern="^[a-zA-Z]+$" maxlength="15" onkeyup="this.value = this.value.toUpperCase();">
+					<span class="icon1"><i class="fa fa-user" aria-hidden="true"></i></span><br>
+					<p id="vlname" style="color:red;"></p>
+				<input placeholder="Description" name="description" class="description" type="text" required="" pattern="^[a-zA-Z]+$" maxlength="15" onkeyup="this.value = this.value.toUpperCase();">
+					<span class="icon2"><i class="fa fa-user" aria-hidden="true"></i></span><br>
+               <p id="vphone" style="color:red;"></p>
+				<input placeholder="quantity" name="quantity"  class="quantity" type="text" required="">
+					<span class="icon3"><i class="fa fa-phone" aria-hidden="true"></i></span><br>
+
+			<p id="vemail" style="color:red;"></p>
+				<input placeholder="price" name="price" class="price" type="text" required="" style="font-size: 10pt; height: 40px; width:320px; ">
+					<span class="icon4"><i class="fa fa-envelope" aria-hidden="true"></i></span></br></br>
+				
+					
+				
+</head>
+<!--<div class="row">
+<label>State:</label><br/>
+<select name="state" id="state-list" class="demoInputBox" onChange="getCity(this.value);">
+<option value="">Select State</option>
+</select>
+</div>-->
+
+<br><br>				 
+					
+<input  placeholder="Password" name="password" class="pass" type="password" required=""></br></br>
+					
+	
+	
+	</div>
+
+		<input type="submit" value="REGISTER">
+</form>
+	
+					
+				
+<script>
+</script>
+
+			</form>
+		</div>
+		<div class="clear"></div>
+</div>
+<!--//main-->
+
+<!--footer-->
+<div class="footer-w3">
+	
+</div>
+-<!--//footer-->
+
+</body>
+</html>
